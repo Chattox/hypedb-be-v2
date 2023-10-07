9bd981d33e746e95da94a65c68f0cbc3fe4c1541
Chattox <conradlineker@gmail.com>
2023-10-07 20:59:52 +0100

    feat: wip addGame mutation

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -5,4 +5,7 @@ export const resolvers = {
     games: async () => await Game.find(),
     gamesByProperty: async (parent, args) => await Game.find({ [args.property]: args.value }),
   },
+  Mutation: {
+    addGame: async (parent, args) => await console.log(args),
+  },
 };

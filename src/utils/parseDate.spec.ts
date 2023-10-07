f687d0fe61b12481e40a8c8117dd36408fe6e789
Chattox <conradlineker@gmail.com>
2023-10-07 22:11:18 +0100

    feat: quarter date parsing

--- src/utils/parseDate.spec.ts
+++ src/utils/parseDate.spec.ts
@@ -12,4 +12,15 @@ describe('parseDate', () => {
       expect(result.displayString).toBe('07/10/2023');
     });
   });
+  describe('quarter date', () => {
+    it('returns releaseDate obj with dateString at the end of the quarter and quarter displayString', () => {
+      const releaseDate = {
+        dateType: 'quarter',
+        dateString: '2023-10-07T17:24:34.000Z',
+      };
+      const result = parseDate(releaseDate);
+      expect(result.dateString).toBe('2023-12-31T23:59:59.999Z');
+      expect(result.displayString).toBe('Q4 2023');
+    });
+  });
 });
--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -1,12 +1,18 @@
 import spacetime from 'spacetime';
 
 export const parseDate = (releaseDate) => {
+  const dateObj = spacetime(releaseDate.dateString);
+
   switch (releaseDate.dateType) {
     case 'specific':
-      const sDateObj = spacetime(releaseDate.dateString);
       return {
-        dateString: sDateObj.format('iso'),
-        displayString: sDateObj.format('numeric-uk'),
+        dateString: dateObj.format('iso'),
+        displayString: dateObj.format('numeric-uk'),
+      };
+    case 'quarter':
+      return {
+        dateString: dateObj.endOf('quarter').format('iso'),
+        displayString: dateObj.format('{quarter} {year}'),
       };
   }
 };

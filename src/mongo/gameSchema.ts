9a7e99d92217f40f1e137520935571d432a8f17d
Chattox <conradlineker@gmail.com>
2023-10-07 18:10:37 +0100

    feat: update gql and mongo schema with new releaseDate shape

--- src/mongo/gameSchema.ts
+++ src/mongo/gameSchema.ts
@@ -3,7 +3,7 @@ import { Schema } from 'mongoose';
 export interface IGame {
   name: string;
   genre?: string;
-  releaseDate: string;
+  releaseDate: object;
   linkUrl: string;
   description?: string;
   hypeScore: number;
@@ -14,7 +14,7 @@ export interface IGame {
 export const gameSchema = new Schema<IGame>({
   name: { type: String, required: true, unique: true },
   genre: { type: String, required: false },
-  releaseDate: { type: String, required: true },
+  releaseDate: { type: Object, required: true },
   linkUrl: { type: String, required: true },
   description: { type: String, required: false },
   hypeScore: { type: Number, required: true },

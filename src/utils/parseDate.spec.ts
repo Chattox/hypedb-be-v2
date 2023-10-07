8356719f99261e3594b0485665a9b14b2b029fe4
Chattox <conradlineker@gmail.com>
2023-10-07 19:24:00 +0100

    feat: parse quarter dates

--- src/utils/parseDate.spec.ts
+++ src/utils/parseDate.spec.ts
@@ -2,7 +2,7 @@ import { parseDate } from './parseDate';
 
 describe('parseDate', () => {
   describe('specific date', () => {
-    it('returns releaseDate object with date and specific dateString', () => {
+    it('returns releaseDate object with dateObj and specific displayString', () => {
       const releaseDate = {
         dateType: 'specific',
         dateString: '2023-10-07T17:24:34+0000',
@@ -10,6 +10,17 @@ describe('parseDate', () => {
       const result = parseDate(releaseDate);
       expect(typeof result.date).toBe('object');
       expect(result.displayString).toEqual('07/10/2023');
+    });
+  });
+  describe('quarter date', () => {
+    it('returns releaseDate object with dateObj and quarter displayString', () => {
+      const releaseDate = {
+        dateType: 'quarter',
+        dateString: '2023-10-07T17:24:34+0000',
+      };
+      const result = parseDate(releaseDate);
+      expect(typeof result.date).toBe('object');
+      expect(result.displayString).toEqual('Q4 2023');
     });
   });
 });
--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -3,10 +3,16 @@ import spacetime from 'spacetime';
 export const parseDate = (releaseDate) => {
   switch (releaseDate.dateType) {
     case 'specific':
-      const dateObj = spacetime(releaseDate.dateString);
+      const sDateObj = spacetime(releaseDate.dateString);
       return {
-        date: dateObj,
-        displayString: dateObj.format('numeric-uk'),
+        date: sDateObj,
+        displayString: sDateObj.format('numeric-uk'),
+      };
+    case 'quarter':
+      const qDateObj = spacetime(releaseDate.dateString);
+      return {
+        date: qDateObj,
+        displayString: qDateObj.format('{quarter} {year}'),
       };
     default:
       return {

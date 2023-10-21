092e80d9840af4e5cc5c14142f64d8649e8ff867
Chattox <conradlineker@gmail.com>
2023-10-21 23:37:30 +0100

    feat(schema): add datetype to db schema

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -24,6 +24,7 @@ export const typeDefs = `#graphql
   type ReleaseDate {
     dateString: String!
     displayString: String!
+    dateType: String!
   }
 
   input GameInput {
--- src/utils/parseDate.ts
+++ src/utils/parseDate.ts
@@ -8,26 +8,31 @@ export const parseDate = (releaseDate) => {
       return {
         dateString: dateObj.format('iso'),
         displayString: dateObj.format('numeric-uk'),
+        dateType: releaseDate.dateType,
       };
     case 'quarter':
       return {
         dateString: dateObj.endOf('quarter').format('iso'),
         displayString: dateObj.format('{quarter} {year}'),
+        dateType: releaseDate.dateType,
       };
     case 'year':
       return {
         dateString: dateObj.endOf('year').format('iso'),
         displayString: dateObj.format('{year}'),
+        dateType: releaseDate.dateType,
       };
     case 'custom':
       return {
         dateString: spacetime('3000-01-01T12:00:00.000Z').format('iso'),
         displayString: releaseDate.dateString,
+        dateType: releaseDate.dateType,
       };
     default:
       return {
         dateString: spacetime('0793-06-08T12:00:00.000Z').format('iso'),
         displayString: 'Invalid date type',
+        dateType: releaseDate.dateType,
       };
   }
 };

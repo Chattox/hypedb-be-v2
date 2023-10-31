cdc087e8a27f2763c3f91cb9f85d6e1fede12781
Chattox <conradlineker@gmail.com>
2023-10-31 14:45:00 +0000

    feat(db): update database connection method for use with production db

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import { connect } from 'mongoose';
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { connect } from "mongoose";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-import '../env';
-import { typeDefs, resolvers } from './schema';
+import "../env";
+import { typeDefs, resolvers } from "./schema";
 
-connect(process.env.DB_CON_STRING || '')
+const dbUrl =
+  process.env.APP_ENV === "production"
+    ? `mongodb+srv://${encodeURIComponent(
+        process.env.DB_USERNAME
+      )}:${encodeURIComponent(process.env.DB_PASSWORD)}${process.env.DB_URL}`
+    : process.env.DB_URL;
+
+connect(dbUrl || "", { dbName: process.env.DB_COLLECTION || undefined })
   .then(() => {
-    console.log('Connected to database');
+    console.log("Connected to database");
   })
   .catch((err) => {
     console.log(err);
   });
 
 const server = new ApolloServer({ typeDefs, resolvers });
-const port = parseInt(process.env.PORT || '4000');
+const port = parseInt(process.env.PORT || "4000");
 
 startStandaloneServer(server, { listen: { port: port } }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
